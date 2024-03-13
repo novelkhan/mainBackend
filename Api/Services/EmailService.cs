312bc7e85e7df291d9e32dc64d91b1226431031d
novelkhan <novel4004@gmail.com>
2024-03-14 05:29:40 +0600

    All Backend files are separated

---
+++ Api/Services/EmailService.cs
@@ -0,0 +1,76 @@
+﻿using Api.DTOs.Account;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit.Text;
+using MimeKit;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+
+namespace Api.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Task<bool> SendEmailAsync(EmailSendDto emailSend)
+        {
+            //MailjetClient client = new MailjetClient(_config["MailJet:ApiKey"], _config["MailJet:SecretKey"]);
+
+            //var email = new TransactionalEmailBuilder()
+            //     .WithFrom(new SendContact(_config["Email:From"], _config["Email:ApplicationName"]))
+            //     .WithSubject(emailSend.Subject)
+            //     .WithHtmlPart(emailSend.Body)
+            //     .WithTo(new SendContact(emailSend.To))
+            //     .Build();
+
+            //var response = await client.SendTransactionalEmailAsync(email);
+            //if (response.Messages != null)
+            //{
+            //    if (response.Messages[0].Status == "success")
+            //    {
+            //        return true;
+            //    }
+            //}
+
+            //return false;
+
+
+
+
+
+
+
+            
+
+
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("data24host@gmail.com"));
+            email.To.Add(MailboxAddress.Parse(emailSend.To));
+            email.Subject = emailSend.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = emailSend.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("data24host@gmail.com", "buximvejccvacrcd");
+            try
+            {
+                smtp.Send(email);
+            }
+            catch (System.Net.Mail.SmtpFailedRecipientException ex)
+            {
+                // ex.FailedRecipient and ex.GetBaseException() should give you enough info.
+                ex.GetBaseException();
+                smtp.Disconnect(true);
+                return Task.FromResult(false);
+            }
+            smtp.Disconnect(true);
+
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
+import { SharedService } from './shared/shared.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private accountService: AccountService,
+    private sharedService: SharedService) {}
+
+  ngOnInit(): void {
+    this.refreshUser();
+  }
+
+  private refreshUser() {
+    const jwt = this.accountService.getJWT();
+    if (jwt) {
+      this.accountService.refreshUser(jwt).subscribe({
+        next: _ => {},
+        error: error => {
+          this.accountService.logout();
+          
+          if (error.status === 401)
+          {
+            this.sharedService.showNotification(false, 'Account blocked', error.error);
+          }
+        }
+      })
+    } else {
+      this.accountService.refreshUser(null).subscribe();
+    }
+  }
+}
